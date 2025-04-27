53ba0f1094197130f1354367c8d7c7db3b417a5e
백세희 <58583422+carrotbyroot77@users.noreply.github.com>
2025-04-27 20:22:47 +0900

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,264 +1,66 @@
 #include <stdio.h>
 int main()
 {
-  int arr[4] = { 10,20,30,40 };
-  int* p = NULL;
+  int n = 10;
+  int n2 = n;
+  int* p = &n;
+  int* p2 = p;
 
-  p = &arr[3];
-
-  printf("%p %p %p %p\n",
-    p, p + 1, p + 2, p + 3);
-  
-  printf("%d %d %d %d\n",
-    p[-3], p[-2], p[-1], p[0]);
-
+  *p2 = 20;
+  printf("%d %d %d\n", n, *p, *p2);
 }
 
 //#include <stdio.h>
 //int main()
 //{
-//  int arr[4] = { 10,20,30,40 };
-//  int* p = NULL;
-//
-//  p = &arr[0];
-//
-//  printf("%p %p %p %p\n",
-//    p, p + 1, p + 2, p + 3);
-//  printf("%d %d %d %d\n",
-//    *p, *(p + 1), *(p + 2), *(p + 3));
-//  printf("%d %d %d %d\n",
-//    p[0], p[1], p[2], p[3]);
-//
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4] = { 10,20,30,40 };
-//  int* p = NULL;
-//
-//  //p = &arr[1];
-//  //p = &arr[3];
-//  p = &arr[0];
-//
-//  printf("%d\n", *p);
-//  printf("%d %d %d %d\n",
-//    arr[0], arr[1], arr[2], arr[3]);
-//  printf("%p %p %p %p\n",
-//    &arr[0], &arr[1], &arr[2], &arr[3]);
-//  printf("%p %p %p %p\n",
-//    p, p+1, p+2, p+3);
-//
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4] = { 10,20,30,40 };
-//  int* p = NULL;
-//
-//  //p = &arr[1];
-//  p = &arr[3];
-//
-//  printf("%d\n", *p);
-//  printf("%d %d %d %d\n",
-//    arr[0], arr[1], arr[2], arr[3]);
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4] = { 10,20,30,40 };
-//  int* p = NULL;
-//
-//  p = &arr[1];
-//
-//  printf("%d\n", *p);
-//  printf("%d %d %d %d\n", 
-//    arr[0], arr[1], arr[2], arr[3]);
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4];
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    arr[i] = (i + 1) * 10;
-//  }
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    printf("%d ", arr[i]);
-//  }
-//
-//  printf("\n");
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4];
-//
-//  for (int i = 0, j =10; i < 4; ++i, j=j+10)
-//  {
-//    arr[i] = j;
-//  }
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    printf("%d ", arr[i]);
-//  }
-//
-//  printf("\n");
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4];
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    arr[i] = 10;
-//  }
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    printf("%d ", arr[i]);
-//  }
-//
-//  printf("\n");
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4];
-//
-//  arr[0] = 10;
-//  arr[1] = 20;
-//  arr[2] = 30;
-//  arr[3] = 40;
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    printf("%d ", arr[i]);
-//  }
-//
-//  printf("\n");
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4] = { 10, 20, 30, 40 };
-//
-//  for (int i = 0; i < 4; ++i)
-//  {
-//    printf("%d ", arr[i]);
-//  }
-//
-//  printf("\n");
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4] = { 10, 20, 30, 40 };
-//
-//  printf("size arr[2] : %d\n", sizeof(arr[2]));
-//  printf("size array : %d\n", sizeof(arr));
-//  printf("%d ", arr[0]);
-//  printf("%d ", arr[1]);
-//  printf("%d ", arr[2]);
-//  printf("%d ", arr[3]);
-//  printf("\n");
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  int arr[4] = { 10, 20, 30, 40 };
-//
-//  printf("%d %d %d %d\n",
-//    arr[0], arr[1], arr[2], arr[3]);
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  // 배열
-//  int a = 10;
-//  int b = 20;
-//  int arr[2] = {10, 20};
-//
-//  printf("%d %d\n", a, b);
-//  printf("%d %d\n", arr[0], arr[1]);
-//
-//}
-
-
-//#include <stdio.h>
-//int main()
-//{
 //  int n = 10;
+//  int n2 = n;
 //  int* p = &n;
+//  int* p2 = p;
+//
 //
 //  printf("%d %d\n", n, *p);
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//  //주소의 연산 : 정수 + 정수 가감산
-//  double n = 10;
-//  double* p = &n;
-//
+//  printf("%p %p\n", &n, &*p);
 //  printf("%p %p\n", &n, p);
-//  printf("%p %p\n", &n + 1, p + 1);
-//  printf("%p %p\n", &n + 2, p + 2);
-//  printf("%p %p\n", &n - 1, p - 1);
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//  //주소의 연산 : 정수 + 정수 가감산
-//  char n = 10;
-//  char* p = &n;
+//  double d;
+//  double d2 = 7.77;
+//  double* pd;
+//  double** ppd;
 //
-//  printf("%p %p\n", &n, p);
-//  printf("%p %p\n", &n + 1, p + 1);
-//  printf("%p %p\n", &n + 2, p + 2);
-//  printf("%p %p\n", &n - 1, p - 1);
-//}
-
-
-//#include <stdio.h>
-//int main()
-//{
-//  //주소의 연산 : 정수 + 정수 가감산
-//  int n = 10;
-//  int* p = &n;
+//  d = 5.55;
+//  pd = &d2;
+//  ppd = &pd;
 //
-//  printf("%p %p\n", &n, p);
-//  printf("%p %p\n", &n + 1, p + 1);
-//  printf("%p %p\n", &n + 2, p + 2);
-//  printf("%p %p\n", &n - 1, p - 1);
+//  printf("%g %g %g\n", d, *pd, **ppd);
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//  //주소의 연산 : 정수 + 정수 가감산
-//  int n = 10;
+//  double d;
+//  double* pd;
+//  double** ppd;
 //
-//  printf("%p %d\n", &n, n);
-//  printf("%p %d\n", &n + 1, n + 1);
-//  printf("%p %d\n", &n + 2, n + 2);
-//  printf("%p %d\n", &n + 3, n + 3);
+//  d = 5.55;
+//  pd = &d;
+//  ppd = &pd;
+//
+//  printf("%g %g %g\n", d, *pd, **ppd);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//  double d = 4.5;
+//  double* pd = &d;
+//  double** ppd = &pd;
+//
+//  printf("%g %g %g\n", d, *pd, **ppd);
 //}
 
 //#include <stdio.h>
@@ -266,181 +68,248 @@ int main()
 //{
 //  int n = 10;
 //  int* p = &n;
+//  int** pp = &p;
 //
-//  *p = 20;
-//
+//  printf("%d %d %d\n", n, *p, **pp);
+//  printf("%p %p %p\n", &n, &*p, &**pp);
+//  printf("%p %p %p\n", &n, p, *pp);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
 //  char c = 10;
-//  char* p2 = &c;
+//  double d = 5.6;
 //
-//  *p2 = 5;
+//  char* p1 = &c;
+//  double* p2 = &d;
 //
-//  double d = 9.99;
-//  double* p3 = &d;
-//
-//  *p3 = 3.141592;
+//  printf("%d %g\n", c, d);
+//  printf("%d %g\n", *p1, *p2);
+//  printf("%d %g\n", *&c, *&d);
 //}
 
+
 //#include <stdio.h>
-//void ResetData(int* p)
-//{
-//  *p = 0;
-//}
-//void PrintData(int data)
-//{
-//  printf("data : % d\n", data);
-//}
-//void IncrementData(int* p)
-//{
-//  *p = *p + 1;
-//}
-//void SetData(int* p, int data)
-//{
-//  *p = data;
-//}
 //int main()
 //{
-//  int n;
-//  ResetData(&n);
-//  PrintData(n);
-//  IncrementData(&n);
-//  PrintData(n);
-//  SetData(&n, 100);
-//  PrintData(n);
-//  IncrementData(&n);
-//  PrintData(n);
+//  char c = 10;
+//  double d = 5.6;
+//
+//  char* p1 = &c;
+//  double* p2 = &d;
+//
+//  printf("%d %g\n", c, d);
+//  printf("%d %g\n", *p1, *p2);
+//  printf("%d %g\n", *&c, *&d);
 //}
 
 //#include <stdio.h>
-//void ResetData(int* p)
-//{
-//  *p = 0;
-//}
-//void PrintData(int data)
-//{
-//  printf("data : % d\n", data);
-//}
-//void IncrementData(int* p)
-//{
-//  *p = *p + 1;
-//}
 //int main()
 //{
-//  int n;
-//  ResetData(&n);
-//  PrintData(n);
-//  IncrementData(&n);
-//  PrintData(n);
+//  char c = 10;
+//  double d = 5.6;
+//
+//  char* p1 = &c;
+//  double* p2 = &d;
+//
+//  printf("%d %g\n", c, d);
+//  printf("%p %p\n", p1, p2);
+//  printf("%p %p\n", &c, &d);
 //}
 
 //#include <stdio.h>
-//void UpdateData(int* p, int data)
-//{
-//  *p = data;
-//}
 //int main()
 //{
 //  int n = 10;
 //  int* k = &n;
 //
-//  printf("data : %d\n", n);
-//  UpdateData(&n, 50);
-//  UpdateData(&*k, 60);
-//  printf("data : %d\n", *k);
+//  n = 100;
+//
+//  printf("%d %d\n", n, *k);
+//  printf("%p %p\n", &n, k);
 //}
 
 //#include <stdio.h>
-//void UpdateData(int* p, int data)
+//int main()
 //{
-//  *p = data;
+//  int n = 10;
+//  int* k = &n;
+//
+//  n = 100;
+//
+//  printf("%d %d\n", n, *k);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//  int n = 10;
+//  int* k = &n;
+//
+//  //n = 100;
+//
+//  printf("%d %d\n", n, *k);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//  int n = 10;
+//  int k = n;
+//
+//  n = 100;
+//
+//  printf("%d %d\n", n, k);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//  int n = 10;
+//  int k = n;
+//
+//  n = 100;
+//
+//  printf("%d %d\n", n, k);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//  //return 0;
+//}
+
+//#include <stdio.h>
+//void PrintA()
+//{
+//  printf("PrintA()\n");
+//}
+//void PrintB()
+//{
+//  PrintA();
+//  printf("PrintB()\n");
+//}
+//void PrintC()
+//{
+//  PrintB();
+//  printf("PrintC()\n");
+//}
+//int main()
+//{
+//  PrintA();
+//  PrintB();
+//  PrintC();
+//}
+
+//#include <stdio.h>
+//void PrintA()
+//{
+//  printf("PrintA()\n");
+//}
+//void PrintB()
+//{
+//  printf("PrintB()\n");
+//}
+//void PrintC()
+//{
+//  PrintB();
+//  printf("PrintC()\n");
+//}
+//int main()
+//{
+//  PrintA();
+//  PrintB();
+//  PrintC();
+//}
+
+//#include <stdio.h>
+//void PrintA()
+//{
+//  printf("PrintA()\n");
+//}
+//void PrintB()
+//{
+//  printf("PrintB()\n");
+//}
+//void PrintC()
+//{
+//  printf("PrintC()\n");
+//}
+//int main()
+//{
+//  PrintB();
+//  PrintB();
+//  PrintB();
+//}
+
+//#include <stdio.h>
+//void PrintA()
+//{
+//  printf("PrintA()\n");
+//}
+//void PrintB()
+//{
+//  printf("PrintB()\n");
+//}
+//void PrintC()
+//{
+//  printf("PrintC()\n");
+//}
+//int main()
+//{
+//  PrintA();
+//  PrintC();
+//  PrintB();
+//}
+
+//#include <stdio.h>
+//int Increment(int k)
+//{
+//  return k+1;
 //}
 //int main()
 //{
 //  int n = 10;
 //
 //  printf("data : %d\n", n);
-//  UpdateData(&n, 200);
+//  n = Increment(n); //++n;
+//  printf("data : %d\n", n);
+//  n = Increment(n); //++n;
+//  printf("data : %d\n", n);
+//  n = Increment(n); //++n;
 //  printf("data : %d\n", n);
 //}
 
 //#include <stdio.h>
-//void UpdateData(int n, int data)
+//int Increment(int k)
 //{
-//  n = data;
+//  return 11;
 //}
 //int main()
 //{
 //  int n = 10;
 //
 //  printf("data : %d\n", n);
-//  UpdateData(n, 200);
+//  n = Increment(n); //++n;
 //  printf("data : %d\n", n);
 //}
 
 //#include <stdio.h>
-//void PrintData(int* p)
-//{
-//  printf("data : %d\n", *p);
-//  *p = 200;
-//}
-//int main()
-//{
-//  int n = 10;
-//  int* p2 = &n;
-//  PrintData(&n);
-//  PrintData(p2);
-//}
-
-
-//#include <stdio.h>
-//void PrintData(int* p)
-//{
-//  printf("data : %d\n", *p);
-//  *p = 200;
-//}
-//int main()
-//{
-//  int n = 10;
-//  int* p2 = &n;
-//  PrintData(&n);
-//  PrintData(p2);
-//}
-
-//#include <stdio.h>
-//void PrintData(int* p)
-//{
-//  printf("data : %d\n", *p);
-//}
-//int main()
-//{
-//  int n = 10;
-//  int* p2 = &n;
-//  PrintData(&n);
-//  PrintData(p2);
-//}
-
-
-//#include <stdio.h>
-//void PrintData(int* p)
-//{
-//  printf("data : %d\n", *p);
-//}
 //int main()
 //{
 //  int n = 10;
 //
-//  PrintData(&n);
-//  PrintData(&n);
+//  printf("data : %d\n", n);
+//  ++n;
+//  printf("data : %d\n", n);
 //}
 
 //#include <stdio.h>
-//void PrintData(int k)
-//{
-//  printf("data : %d\n", k);
-//}
 //int main()
 //{
 //  int n = 10;
 //
-//  PrintData(n);
-//  PrintData(n);
+//  printf("data : %d\n", n);
 //}
